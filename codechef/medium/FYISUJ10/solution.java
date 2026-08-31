7f45b2538f9f4c738e6f87619eaacfae7acbaed2
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:15:00 +0530

    ✅ FYISUJ10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/FYISUJ10/solution.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign temperature readings
+        int hotRegionTemp=45;
+        int coldRegionTemp=-20;
+
+
+
+        //  Print the temperature readings
+        System.out.println("Hot Region Temperature:"+hotRegionTemp+"\n"+"Cold Region Temperature:"+coldRegionTemp );
+
+
+    }
+}
