5357dc1898841cf7020ad2c6fca93e5d26cba727
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:02:18 +0530

    ✅ Sum of Squares of First n Natural Numbers | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ gfg/easy/sum-of-squares-of-first-n-natural-numbers/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    // Function to calculate the sum of squares of first 'number' natural numbers
+    public int sumOfSquares(int n) {
+        // code here
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+=i*i;
+        }
+        return ans;
+    }
+}
