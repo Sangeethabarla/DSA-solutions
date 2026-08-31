c2198392e0d5ec12f64d908d548e75861684cd29
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:18:27 +0530

    ✅ FYISUJ15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/FYISUJ15/solution.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main (String[] args) {
+        // Declare and assign earthquake intensity readings
+        short regionOneIntensity = 12000;
+        short regionTwoIntensity = -5000;
+        byte regionThreeIntensity = 0;
+
+        // Print the intensity values
+        System.out.println("Region One Intensity: " + regionOneIntensity);
+        System.out.println("Region Two Intensity: " + regionTwoIntensity);
+        System.out.println("Region Three Intensity: " + regionThreeIntensity);
+    }
+}
