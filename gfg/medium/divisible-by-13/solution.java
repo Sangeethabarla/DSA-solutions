230864d89b85679639cb8f802548c69edd93c69a
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:07:47 +0530

    ✅ Divisible by 13 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ gfg/medium/divisible-by-13/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean divby13(String s) {
+        int remainder = 0;
+        for (int i = 0; i < s.length(); i++) {
+            remainder = (remainder * 10 + (s.charAt(i) - '0')) % 13;
+        }
+        return remainder == 0;
+    }
+}
