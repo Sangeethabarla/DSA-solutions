710c8675bdda09a4f2076086ea29203fe4b88de8
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:23:47 +0530

    ✅ FYISUJ26 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/FYISUJ26/solution.java
@@ -0,0 +1,10 @@
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+        long transactionID = 5_123_456_789_000L;  
+        long branchCode = 3052;  
+        
+        System.out.println("Transaction ID:"+transactionID);
+        System.out.println("Branch Code:"+branchCode);
+    }
+}
