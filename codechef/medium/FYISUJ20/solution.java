d3e1e328bbe408c0e77a83953b5decb1737d07db
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-08-31 22:17:12 +0530

    ✅ FYISUJ20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/FYISUJ20/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main (String[] args) {
+        // Declare and assign population values
+        int cityPopulation = 5_000_000;
+        short districtPopulation = 25_000;
+
+        // Print the population values
+        System.out.println("City Population: " + cityPopulation);
+        System.out.println("District Population: " + districtPopulation);
+    }
+}
