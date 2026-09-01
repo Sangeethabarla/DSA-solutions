5cdac15fa97228d3831c49fe6cb1eded225371c6
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-01 12:22:55 +0530

    ✅ Climbing Stairs | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.2 MB | 2026-09-01

---
+++ leetcode/easy/0070-climbing-stairs/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int n1=2,n2=1;
+        for(int i=3;i<=n;i++){
+            int curr=n1+n2;
+            n2=n1;
+            n1=curr;
+        }
+        return n1;
+    }
+}
