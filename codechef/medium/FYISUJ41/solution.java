5f635396117eb5a42df098c54b2f25186f3f679c
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-02 17:46:29 +0530

    ✅ FYISUJ41 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/FYISUJ41/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign mission details
+        char a='A';
+        int crew=7;
+        long distance=3000000000l;
+
+        // Print the mission data
+        
+        System.out.println("Mission Code:"+a);
+        System.out.println("Crew Size:"+crew);
+        System.out.println("Distance Traveled:"+distance+"km");
+
+
+
+    }
+}
