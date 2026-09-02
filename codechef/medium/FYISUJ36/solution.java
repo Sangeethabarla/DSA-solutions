080822fc1fcb36c01942aaeaac0c1b3047d92bb6
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-02 17:40:29 +0530

    ✅ FYISUJ36 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/FYISUJ36/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Step 1: Declare and assign temperature (float)
+        float Temperature=28.3f;
+        float RainfallLevel=125.6789f;
+
+
+        // Step 2: Declare and assign rainfall level (double)
+
+
+        // Step 3: Print the weather data
+        System.out.println("Temperature: " + Temperature + " °C");
+        System.out.println("Rainfall Level: " + RainfallLevel + " mm");
+
+    }
+}
