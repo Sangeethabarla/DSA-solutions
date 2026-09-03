9552c8ca338ad5ecb01ce0c30c555a5af642a5a1
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-03 10:31:01 +0530

    ✅ FYISUJ45 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/FYISUJ45/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign deep-sea exploration data
+        
+        float x=3452.78f;
+        boolean y=true;
+
+
+
+        // Print the exploration data
+        
+        System.out.println("Depth Reached:"+x+"meters");
+        System.out.println("Vehicle Exploring:"+y);
+
+
+    }
+}
