f796b4f2043944fac55f45e2eaff44db2587c8e8
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-08 17:34:07 +0530

    ✅ TONKSS30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

--- codechef/medium/TONKSS30/solution.java
+++ codechef/medium/TONKSS30/solution.java
@@ -1,11 +1,15 @@
 class Codechef {
     public static void main(String[] args) {
-        
-        // Declaring and assigning a fixed tax rate
-        final float FIXED_RATE = 0.18f;
+        // Declare and assign a fixed speed limit using final
+            final int SPEED_LIMIT=80;
+            
 
-        // Printing the Fixed Tax Rate
-        System.out.println("Fixed Tax Rate: " + FIXED_RATE);
+        // Declare and assign current speed
+            int currentSpeed=75;
 
+
+        // Print the speed limit and current speed
+        System.out.println("Speed Limit: " + SPEED_LIMIT + " km/h");
+        System.out.println("Current Speed: " + currentSpeed + " km/h");
     }
 }
