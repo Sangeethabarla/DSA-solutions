0475a4f8bc217230c7b2eae27094d897110f43c7
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-09 18:32:28 +0530

    ✅ ArrayList Iterate Front to Back | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ gfg/easy/arraylist-iterate-front-to-back/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+
+    public static void iterateArrayList(Iterator iterator) {
+        // Your code here
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
+    
+}
