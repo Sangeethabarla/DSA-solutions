9b5b2dbbed596108e2e0da04997133e83f1d46f1
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-09 18:02:42 +0530

    ✅ Most Common Word | Easy | Java | Runtime: 19 ms (23.00%) | Memory: 45.3 MB | 2026-09-09

---
+++ leetcode/easy/0819-most-common-word/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String>s=new HashSet<>(Arrays.asList(banned));
+        String[] words = paragraph.replaceAll("[^a-zA-Z]", " ").toLowerCase().split("\\s+");
+
+
+        HashMap<String,Integer>hm=new HashMap<>();
+        for(String word:words){
+            if(!s.contains(word)){
+                hm.put(word,hm.getOrDefault(word,0)+1);
+            }
+        }
+        String maxword=" ";
+        int maxcount=0;
+        for(Map.Entry<String,Integer>entry:hm.entrySet()){
+            if(entry.getValue()>maxcount){
+                maxcount=entry.getValue();
+                maxword=entry.getKey();
+            }
+        }
+        return maxword;
+
+
+
+    }
+}
