c67bb9555b7d4c4b78c32f15fe76a0f06b4daff2
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-09 18:40:06 +0530

    ✅ CRQDWE06 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/CRQDWE06/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Write your code here
+        int pushups = 40, situps = 30, squats = 20;
+        
+        System.out.println(pushups+situps+squats);
+        
+    }
+}
