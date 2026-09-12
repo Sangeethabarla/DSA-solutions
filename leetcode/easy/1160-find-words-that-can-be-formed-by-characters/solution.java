38a3cc7b2725afa47382b7621d8a3ff701d983ae
Sangeetha Balavalli Barla <sangeethabarla11@gmail.com>
2026-09-12 22:43:44 +0530

    ✅ Find Words That Can Be Formed by Characters | Easy | Java | Runtime: 6 ms (88.41%) | Memory: 47.5 MB | 2026-09-12

---
+++ leetcode/easy/1160-find-words-that-can-be-formed-by-characters/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] charCounts = new int[26];
+        for (char c : chars.toCharArray()) {
+            charCounts[c - 'a']++;
+        }
+        
+        int totalLength = 0;
+        
+        for (String word : words) {
+            int[] wordCounts = new int[26];
+            boolean isGood = true;
+            for (char c : word.toCharArray()) {
+                wordCounts[c - 'a']++;
+                if (wordCounts[c - 'a'] > charCounts[c - 'a']) {
+                    isGood = false;
+                    break;
+                }
+            }
+            
+            if (isGood) {
+                totalLength += word.length();
+            }
+        }
+        
+        return totalLength;
+    }
+}
